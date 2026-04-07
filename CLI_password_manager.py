a91de520be710c8b92675ca104b2bb3768e5e511
osamuflair <osamudiamenbenedict05@gmail.com>
2026-04-07 21:57:17 +0100

    week 2 project skeleton: CLI Password Manager

---
+++ CLI_password_manager.py
@@ -0,0 +1,14 @@
+# CLI Password Manager
+
+class PasswordManager:
+    def __init__(self):
+        pass
+    
+    def add_password(self):
+        pass
+    
+    def view_passwords(self):
+        pass
+    
+    def delete_password(self):
+        pass
