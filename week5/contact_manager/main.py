11acba9a17b5598ca922af60221faa34ca0ce141
osamuflair <osamudiamenbenedict05@gmail.com>
2026-04-23 21:36:34 +0100

    Remove pycache and add to gitignore

--- week5/contact_manager/main.py
+++ week5/contact_manager/main.py
@@ -12,13 +12,15 @@ contact_list = [{'id':1, 'name':'Osamu', 'phone':'07089646092', 'email':'osamu10
 
 @app.get('/contacts')
 def get_contact():
+    """gets all contact from the database(contact_list)"""
     return contact_list
 
 @app.post('/contacts/')
 def create_contact(contact: Contact):
+    """creates a new contact"""
     item = contact_list[-1]
     num = item["id"] + 1
-    new_contact = contact.model_dump()
+    new_contact = contact.model_dump()#convert to a dictionary
     new_contact["id"] = num
     contact_list.append(new_contact)
     return({"message": "successful created", "contact": contact_list})
