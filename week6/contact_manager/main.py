4b1a5ddbf4a418200ec9e2c567a8fa11a553b624
osamuflair <osamudiamenbenedict05@gmail.com>
2026-04-26 22:55:57 +0100

    Initialize week 6 authentication project with user models

--- week6/contact_manager/main.py
+++ week6/contact_manager/main.py
@@ -1,3 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+users = {
+    'osamu_1':{
+        'username': 'osamu_1',
+        'email': 'osamu_1@gmail.com',
+        'hashed_password': '83h$6jsdsdu^,;.dskaojedjdr#d%'
+    }
+}
+
+class User(BaseModel):
+    username: str
+    email: str
+
+class UserInDb(User):
+    hashed_password: str
+
