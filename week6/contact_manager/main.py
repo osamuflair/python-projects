f739690ffbaecd0c149a25cba1fdc98db1635212
osamuflair <osamudiamenbenedict05@gmail.com>
2026-04-26 23:22:14 +0100

    Add password hashing and user authentication functions

--- week6/contact_manager/main.py
+++ week6/contact_manager/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pwdlib import PasswordHash
+
+pwd_context = PasswordHash.recommended()
+DUMMY_HASH = pwd_context.hash('DUMMY PASSWORD')
 
 app = FastAPI()
 
@@ -18,3 +22,26 @@ class User(BaseModel):
 class UserInDb(User):
     hashed_password: str
 
+def hash_password(password):
+    """hashes a password"""
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    """checks if a password is same with the hashed password in database"""
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_user(username):
+    """checks if user is in database"""
+    if username in users:
+        user_dict = users[username]
+        return UserInDb(**user_dict)
+    
+def authenticate_user(username, password):
+    """checks if a user exists and if the password is correct"""
+    user = get_user(username)
+    if not user:
+        verify_password(password, DUMMY_HASH)
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
